2f7f315c0fec323f90f024f744dbe5db3ad27cbc
GaydaR <ronald.gayda.13@gmail.com>
2019-06-03 09:37:25 -0700

    Added exception for db config variable

--- startup/config.js
+++ startup/config.js
@@ -4,4 +4,7 @@ module.exports = function(){
     if(!config.get('jwtPrivateKey')){
         throw new Error('FATAL ERROR; jwtPrivateKey is not defined.');
     }
+    if(!config.get('db')){
+        throw new Error('FATAL ERROR: db is not defined.');
+    }
 };
