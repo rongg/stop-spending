7129ca97374df375016461cbd128528ab8dca26c
GaydaR <ronald.gayda.13@gmail.com>
2019-06-08 11:35:32 -0700

    Enable cors

--- startup/routes.js
+++ startup/routes.js
@@ -5,11 +5,13 @@ const auth = require('../routes/auth');
 const error = require('../middleware/error');
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 
 module.exports = function(app){
     app.use(express.json());
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(error);
+    app.use(cors());
     app.use('/api/habits', habits);
     app.use('/api/expenses', expenses);
     app.use('/api/users', users);
