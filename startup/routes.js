224edab7e0c1246cd880a26d564f572aef88da02
GaydaR <ronald.gayda.13@gmail.com>
2019-06-10 11:06:17 -0700

    Expose auth headers for cors options

--- startup/routes.js
+++ startup/routes.js
@@ -7,11 +7,15 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
+const corsOptions = {
+    exposedHeaders: 'x-auth-token',
+};
+
 module.exports = function(app){
     app.use(express.json());
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(error);
-    app.use(cors());
+    app.use(cors(corsOptions));
     app.use('/api/habits', habits);
     app.use('/api/expenses', expenses);
     app.use('/api/users', users);
