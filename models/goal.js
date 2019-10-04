df375a79ae419213d17fc94772268ed08d063310
GaydaR <ronald.gayda.13@gmail.com>
2019-10-04 10:21:39 -0700

    Create goal model and unit tests

---
+++ models/goal.js
@@ -0,0 +1,39 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const goalSchema = new mongoose.Schema({
+    userId: {type: String, maxLength: 25, require: true},
+    start: {type: Date, require: true},
+    end: {type: Date, require: true},
+    habitId: {type: String, max: 25, require: true},
+    type: {type: String, max: 25, require: true},
+    target: {type: Number, require: true},
+    pass: {type: Boolean, default: false},
+    active: {type: Boolean, default: true}
+});
+
+const Goal = mongoose.model('Goal', goalSchema);
+
+const validation = {
+    check: function (goal) {
+        const jGoalSchema = {
+            _id: Joi.string(),
+            userId: Joi.string().max(25).required(),
+            start: Joi.date().required(),
+            end: Joi.date().required(),
+            habitId: Joi.string().max(25).required(),
+            type: Joi.string().max(25).required(),
+            target: Joi.number().required(),
+            pass: Joi.boolean(),
+            active: Joi.boolean(),
+        };
+
+        return Joi.validate(goal, jGoalSchema);
+    },
+    checkId: mongoose.Types.ObjectId.isValid
+};
+
+module.exports = {
+    Goal,
+    validation: validation
+};
---
+++ tests/unit/models/goal.test.js
@@ -0,0 +1,142 @@
+const {validation} = require('../../../models/goal');
+
+describe('goal.validation.check', () => {
+    const validGoal = {
+        userId: "12345",
+        start: new Date(),
+        end: new Date(),
+        habitId: "12345",
+        type: 'micro_budget',
+        pass: false,
+        active: true
+
+    };
+    let result;
+    describe('userId', () => {
+        it('should exist', () => {
+            const goal = Object.assign({}, validGoal);
+            delete goal.userId;
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+        it('should be a string', () => {
+            const goal = Object.assign({}, validGoal);
+            goal.userId = 12345;
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+        it('should be less than 25 characters', () => {
+            const goal = Object.assign({}, validGoal);
+            goal.userId = "12345678901234567890123456789";
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+    });
+    describe('start', () => {
+        it('should exist', () => {
+            const goal = Object.assign({}, validGoal);
+            delete goal.start;
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+        it('should be a valid date string or millisecond value', () => {
+            const goal = Object.assign({}, validGoal);
+            goal.start = "abc";
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+    });
+    describe('end', () => {
+        it('should exist', () => {
+            const goal = Object.assign({}, validGoal);
+            delete goal.end;
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+        it('should be a valid date string or millisecond value', () => {
+            const goal = Object.assign({}, validGoal);
+            goal.end = "abc";
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+    });
+    describe('habitId', () => {
+        it('should exist', () => {
+            const goal = Object.assign({}, validGoal);
+            delete goal.habitId;
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+        it('should be a string', () => {
+            const goal = Object.assign({}, validGoal);
+            goal.habitId = 12345.5;
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+        it('should be less than 25 characters', () => {
+            const goal = Object.assign({}, validGoal);
+            goal.habitId = "12345678901234567890123456789";
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+    });
+    describe('type', () => {
+        it('should exist', () => {
+            const goal = Object.assign({}, validGoal);
+            delete goal.type;
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+        it('should be a string', () => {
+            const goal = Object.assign({}, validGoal);
+            goal.type = 12345.5;
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+        it('should be less than 25 characters', () => {
+            const goal = Object.assign({}, validGoal);
+            goal.type = "12345678901234567890123456789";
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+    });
+    describe('target', () => {
+        it('should exist', () => {
+            const goal = Object.assign({}, validGoal);
+            delete goal.target;
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+        it('should be a number', () => {
+            const goal = Object.assign({}, validGoal);
+
+            goal.target = 10.5;
+            result = validation.check(goal);
+            expect(result.error).toBeNull();
+
+            goal.target = 10;
+            result = validation.check(goal);
+            expect(result.error).toBeNull();
+
+            goal.target = "abcdef";
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+    });
+    describe('pass', () => {
+        it('should be a boolean', () => {
+            const goal = Object.assign({}, validGoal);
+            goal.pass = "bad";
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+    });
+    describe('active', () => {
+        it('should be a boolean', () => {
+            const goal = Object.assign({}, validGoal);
+            goal.active = "bad";
+            result = validation.check(goal);
+            expect(result.error).toBeTruthy();
+        });
+    });
+});
