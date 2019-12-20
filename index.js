fb7f838aff81df429be1be94df8c4ad758e8233b
GaydaR <ronald.gayda.13@gmail.com>
2019-12-20 10:05:52 -0800

    Add goal job to project

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 const express = require('express');
 const app = express();
 const winston = require('winston');
+const goalJob = require('./jobs/goal_job');
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
@@ -12,15 +13,15 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
 
+console.log('ENV', process.env.NODE_ENV);
 
 const port = process.env.PORT;
 const server = app.listen(port, () => {
     let message = `App listening on port ${server.address().port}...`;
     console.info(message);
     winston.info(message);
+    if(process.env.NODE_ENV !== 'test') goalJob();
 });
 
 
 module.exports = server;
-
-
---
+++ jobs/goal_job.js
@@ -0,0 +1,39 @@
+const Agenda = require('agenda');
+const config = require('config');
+
+const dbConn = config.get('db');
+const dbName = dbConn.substring(dbConn.lastIndexOf('/') + 1);
+
+const MongoClient = require('mongodb').MongoClient;
+
+const agenda = new Agenda({db: {address: dbConn}});
+
+agenda.define('update goal statuses', async job => {
+    console.log('update goals');
+    MongoClient.connect(dbConn, async (err, client) => {
+        if (err) {
+            return console.error(err);
+        }
+        const db = client.db(dbName);
+        const goals = db.collection('goals');
+        let now = new Date();
+
+        const update = await goals.updateMany(
+            {end: {$lte: now}, active: true},
+            {$set: {active: false}}
+        );
+
+        console.log("found: ", update.matchedCount);
+        console.log("modified: ", update.modifiedCount);
+
+        client.close();
+    });
+
+});
+
+
+module.exports = (async function () {
+    console.log('goal job db: ', dbConn, 'name: ', dbName);
+    await agenda.start();
+    await agenda.every('1 minutes', 'update goal statuses');
+});
